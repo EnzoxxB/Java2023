b5b6b7c686a0df919f62e160a9b62114b0dd6c9f
Enzo Balderrama <enzoxxb@gmail.com>
2023-08-21 15:36:43 -0300

    Commit primer clase de java2, Ciclos While, DoWhile y For.

---
+++ Lección2/CicloWhile.java
@@ -0,0 +1,19 @@
+package Lección2;
+
+public class CicloWhile {
+    public static void main(String[] args) {
+        var conteo = 0; // inferencia de tipos
+        while (conteo <= 3){
+            System.out.println("conteo = "+ conteo);
+            conteo++;
+        }
+        var contador = 0;
+        do {
+            System.out.println("contador = "+ contador);
+            contador++;
+        } while(contador < 4);
+        for(var i = 0; i <= 7; i++){
+            System.out.println("contador = "+ i);
+        }
+    }
+}
